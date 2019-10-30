95ef64b706373b34228d162494bb74128278162e
qunge <zhangguoqiang@wowkai.cn>
2019-10-30 14:40:56 +0800

    qiniu云存储

---
+++ src/main/java/cn/stylefeng/guns/modular/api/QiniuController.java
@@ -0,0 +1,46 @@
+package cn.stylefeng.guns.modular.api;
+
+import cn.stylefeng.guns.core.service.QiniuService;
+import com.qiniu.http.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/qiniu")
+public class QiniuController {
+
+    @Autowired
+    private QiniuService qiniuService;
+
+    /**
+     * 以流的形式上传图片
+     *
+     * @param file
+     * @return 返回访问路径
+     * @throws IOException
+     */
+    @PostMapping("upload")
+    public String uploadFile(@RequestParam(value = "file") MultipartFile file) throws IOException {
+        return qiniuService.uploadFile(file.getInputStream());
+    }
+
+    /**
+     * 删除文件
+     *
+     * @param key
+     * @return
+     * @throws IOException
+     */
+    @GetMapping("delete/{key}")
+    public Response deleteFile(@PathVariable String key) throws IOException {
+        return qiniuService.delete(key);
+    }
+}
