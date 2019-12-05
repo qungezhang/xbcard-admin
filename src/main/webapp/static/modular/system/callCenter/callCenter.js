5cbb16624ec5611110c458f5ca2be59e30ee74dd
qunge <zhangguoqiang@wowkai.cn>
2019-12-05 19:44:07 +0800

    客服中心 页面

--- src/main/webapp/static/modular/system/callCenter/callCenter.js
+++ src/main/webapp/static/modular/system/callCenter/callCenter.js
@@ -14,21 +14,62 @@ var CallCenter = {
 CallCenter.initColumn = function () {
     return [
         {field: 'selectItem', radio: true},
-            {title: '主键id', field: 'id', visible: true, align: 'center', valign: 'middle'},
+            {title: '主键id', field: 'id', visible: false, align: 'center', valign: 'middle'},
             {title: '问题内容', field: 'content', visible: true, align: 'center', valign: 'middle'},
             {title: '电话', field: 'mobile', visible: true, align: 'center', valign: 'middle'},
-            {title: '内容图片', field: 'contentImg', visible: true, align: 'center', valign: 'middle'},
+            // {title: '内容图片', field: 'contentImg', visible: true, align: 'center', valign: 'middle'},
             {title: '回复内容', field: 'replyContent', visible: true, align: 'center', valign: 'middle'},
             {title: '回复时间', field: 'replyTime', visible: true, align: 'center', valign: 'middle'},
-            {title: '用户id', field: 'userId', visible: true, align: 'center', valign: 'middle'},
+            // {title: '用户id', field: 'userId', visible: true, align: 'center', valign: 'middle'},
             {title: '状态（0处理中，1已处理）', field: 'status', visible: true, align: 'center', valign: 'middle'},
-            {title: '评估等级', field: 'valuationLevel', visible: true, align: 'center', valign: 'middle'},
-            {title: '是否删除（0否，1是）', field: 'isDeleted', visible: true, align: 'center', valign: 'middle'},
+            // {title: '评估等级', field: 'valuationLevel', visible: true, align: 'center', valign: 'middle'},
+            // {title: '是否删除（0否，1是）', field: 'isDeleted', visible: true, align: 'center', valign: 'middle'},
             {title: '创建时间', field: 'createTime', visible: true, align: 'center', valign: 'middle'},
-            {title: '创建人', field: 'createBy', visible: true, align: 'center', valign: 'middle'},
-            {title: '修改时间', field: 'updateTime', visible: true, align: 'center', valign: 'middle'},
-            {title: '修改人', field: 'updateBy', visible: true, align: 'center', valign: 'middle'}
+            {title: '操作', field: 'Button', visible: true, align: 'center', valign: 'middle',
+            formatter: operateFormatter,events: operateEvents
+            },
+            // {title: '创建人', field: 'createBy', visible: true, align: 'center', valign: 'middle'},
+            // {title: '修改时间', field: 'updateTime', visible: true, align: 'center', valign: 'middle'},
+            // {title: '修改人', field: 'updateBy', visible: true, align: 'center', valign: 'middle'}
     ];
+};
+
+function operateFormatter(value, row, index) {
+    if (row.status == 0) {
+        return [
+            '<input type="button" value="编辑" id="Ofedit" class="btn btn-primary btn-sm"   data-toggle="modal" style="display:inline">'
+        ].join('');
+    } else {
+        return [
+            '<input type="button" value="查看" id="Ofinfo" class="btn btn-primary btn-sm"   data-toggle="modal" style="display:inline"> ',
+        ].join('');
+    }
+
+}
+
+window.operateEvents = {
+    'click #Ofinfo': function (e, value, row, index) {
+        var index = layer.open({
+            type: 2,
+            title: '客服中心记录详情',
+            area: ['800px', '420px'], //宽高
+            fix: false, //不固定
+            maxmin: true,
+            content: Feng.ctxPath + '/callCenter/callCenter_update/' + row.id
+        });
+        this.layerIndex = index;
+    },
+    'click #Ofedit': function (e, value, row, index) {
+        var index = layer.open({
+            type: 2,
+            title: '客服中心记录详情',
+            area: ['800px', '420px'], //宽高
+            fix: false, //不固定
+            maxmin: true,
+            content: Feng.ctxPath + '/callCenter/callCenter_update/' + row.id
+        });
+        this.layerIndex = index;
+    }
 };
 
 /**
