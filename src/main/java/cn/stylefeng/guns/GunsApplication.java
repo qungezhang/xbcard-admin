1d57a3461730c9f4922aa74a5f5b8f711a91e06f
qunge <247862915@qq.com>
2020-08-03 11:20:51 +0800

    解决文件上传,临时文件夹被程序自动删除问题

--- src/main/java/cn/stylefeng/guns/GunsApplication.java
+++ src/main/java/cn/stylefeng/guns/GunsApplication.java
@@ -20,6 +20,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * SpringBoot方式启动类
@@ -27,7 +32,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author stylefeng
  * @Date 2017/5/21 12:06
  */
-@SpringBootApplication(exclude = WebAutoConfiguration.class)
+@SpringBootApplication(exclude = {WebAutoConfiguration.class, MultipartAutoConfiguration.class})
 public class GunsApplication {
 
     private final static Logger logger = LoggerFactory.getLogger(GunsApplication.class);
@@ -35,5 +40,18 @@ public class GunsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GunsApplication.class, args);
         logger.info("GunsApplication is success!");
+    }
+    /**
+     * 解决文件上传,临时文件夹被程序自动删除问题
+     *
+     * 文件上传时自定义临时路径
+     * @return
+     */
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //2.该处就是指定的路径(需要提前创建好目录，否则上传时会抛出异常)
+        factory.setLocation("/opt/uploadtmp");
+        return factory.createMultipartConfig();
     }
 }
